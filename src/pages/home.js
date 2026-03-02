3d63ab90a1226127daa1b260641bf125d0f83fe7
Dawit <208569702+dawit-kibatu@users.noreply.github.com.>
2026-03-02 20:17:33 +0300

    fetch: get stored tasks and display them

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,8 @@
 import { mdiFunctionVariant } from "@mdi/js";
 import "../styles/style.css";
 let home=function(){
-    let content=document.querySelector("#content");
+    let tasks=document.querySelector(".tasks");
+    getTasks()
     function createTask(name,duedate){
         return{
             name,
@@ -10,7 +11,7 @@ let home=function(){
         }
     //stores the task in localstorage
     function storeTask(task){
-        localStorage.setItem(`${task.name}`,JSON.stringify(task))
+        localStorage.setItem(`${task.name}`,JSON.stringify(task));
     }
     //removes the task from the local storage
     function deleteTask(task){
@@ -29,6 +30,7 @@ let home=function(){
         storeTask(task);
         popup.classList.remove("openPop");
         clearInputField();
+        displayTasks(task)
     })
     let cancelBtn=document.querySelector("#cancel");
     cancelBtn.addEventListener("click",()=>{
@@ -38,6 +40,24 @@ let home=function(){
     function clearInputField(){
          document.querySelector("#name").value=""
          document.querySelector("#duedate").value=""
-}
+        }
+    //displays the tasks on the page
+    function displayTasks(task){
+        let div=document.createElement("div");
+        div.classList.add("task");
+        div.innerHTML=`
+        <div>${task.name}<div/>
+        <div>${task.duedate}<div/>
+        `
+        tasks.appendChild(div)
+    }
+    //gets the tasks from localStorage and prints them out on the page
+    function getTasks(){
+        const keys=Object.keys(localStorage);
+        keys.map((key)=>{
+            let task=JSON.parse(localStorage.getItem(key));
+            displayTasks(task)
+        })
+    }
 }
 export default home
