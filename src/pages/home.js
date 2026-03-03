02416aff1b314be8f388bdf94cc639269ce39a34
Dawit <208569702+dawit-kibatu@users.noreply.github.com.>
2026-03-03 11:48:17 +0300

    style: minor adjustment here and there

--- src/pages/home.js
+++ src/pages/home.js
@@ -19,18 +19,24 @@ let home=function(){
     }
     let popup=document.querySelector(".popup");
     let addBtn=document.querySelector(".add");
+    let sideAddBtn=document.querySelector("#addBtn")
     let submitBtn=document.querySelector("#submitBtn")
     addBtn.addEventListener("click",()=>{
+        popup.classList.add("openPop");
+    })
+    sideAddBtn.addEventListener("click",()=>{
         popup.classList.add("openPop");
     })
     submitBtn.addEventListener("click",()=>{
         let taskName=document.querySelector("#name").value;
         let dueDate=document.querySelector("#duedate").value;
+        if(dueDate!==""){
         let task=createTask(taskName,dueDate);
         storeTask(task);
         popup.classList.remove("openPop");
         clearInputField();
         displayTasks(task)
+        }     
     })
     let cancelBtn=document.querySelector("#cancel");
     cancelBtn.addEventListener("click",()=>{
@@ -45,17 +51,30 @@ let home=function(){
     function displayTasks(task){
         let div=document.createElement("div");
         div.classList.add("task");
-        div.innerHTML=`
-        <div>${task.name}</div>
-        <div>${task.duedate}</div>
-        `
+        let taskname=document.createElement("div")
+        taskname.textContent=task.name;
+        taskname.classList.add("taskname");
+        // div.innerHTML=`
+        // <div class="taskname">${task.name}</div>
+        // `
+        // <div>${task.duedate}</div>
+
         let dltBtn=document.createElement("button");
+        dltBtn.classList.add("dltBtn")
         dltBtn.addEventListener("click",()=>{
              div.remove()
              deleteTask(task);
         })
+        div.appendChild(taskname)
         div.appendChild(dltBtn)
         tasks.appendChild(div)
+        dltBtn.addEventListener("mouseenter",()=>{
+            taskname.style.cssText=`text-decoration: line-through;`
+        })
+        dltBtn.addEventListener("mouseleave",()=>{
+            taskname.style.cssText=`text-decoration: none;`
+        })
+
     }
     //gets the tasks from localStorage and prints them out on the page
     function getTasks(){
